5e60c1faf2e4b11b374bc4002d1e5bc83847692f
Ponca Labor01 <polbr01@safeway.com>
2022-12-20 22:13:30 -0600

    Initial Commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+const Head = () => {
+	return (
+		<>
+			<title>Sanity Blog</title>
+			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	);
+};
+export default Head;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+	return (
+		<div>
+			<h1 className="text-3xl">Hello</h1>
+		</div>
+	);
+};
+export default HomePage;
